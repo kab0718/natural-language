32ced3e053d309be5bdb7ffcdfbee77fc7573610
kab0718 <kab3nan9@gmail.com>
2019-07-12 11:23:17 +0900

    分かち書きverを追加&Bag of wordsによる文字のベクトル化実装

--- application/sharing.py
+++ application/sharing.py
@@ -2,8 +2,8 @@ import MeCab
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
-def split_text_noun(text):
-    tagger = MeCab.Tagger("-Ochasen -d C:\mecab-ipadic-neologd")
+def sharing_text(text):
+    tagger = MeCab.Tagger("-Owakati -d C:\mecab-ipadic-neologd")
 
     tagger.parse("")    #エラー回避のために空文字をパース
     node = tagger.parseToNode(text)     #最初のnodeを取得
@@ -12,20 +12,18 @@ def split_text_noun(text):
 
     while node:
         word = node.surface     #surfaceには単語が入っている
-        hinsi = node.feature.split(",")[0]      #featureには品詞や品詞細分類,活用形,読み方など様々な情報が入っている
-        if hinsi == "名詞":
-            words.append(word)
+        words.append(word)
         node = node.next    #次のnodeに移る
 
     return words    #名詞だけのリストを返す
 
 text = "令和元年硬貨と天皇陛下即位の記念硬貨の打ち初め式が、大阪市北区の造幣局で行われました"
-split_text = ' '.join(split_text_noun(text))    #リストを半角スペースごとに結合
-print(split_text)
+wakati_text = ' '.join(sharing_text(text))    #リストを半角スペースごとに結合
+print(wakati_text)
 
 message_list = [
-    ' '.join(split_text_noun("私達はラーメンがとても大好きです。")),
-    ' '.join(split_text_noun("私達は蕎麦がとても大好きです。"))
+    ' '.join(sharing_text("私達はラーメンがとても大好きです。")),
+    ' '.join(sharing_text("私達は蕎麦がとても大好きです。"))
 ]
 
 docs = np.array(message_list)
