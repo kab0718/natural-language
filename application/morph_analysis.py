6da90874bbb23f1b3f8dd2691fe3a434f1b026ff
kab0718 <kab3nan9@gmail.com>
2019-07-12 10:30:15 +0900

    名詞だけのリストを返す関数作成

--- application/morph_analysis.py
+++ application/morph_analysis.py
@@ -6,12 +6,17 @@ def split_text_noun(text):
     tagger.parse("")
     node = tagger.parseToNode(text)
 
+    words = []
+
     while node:
         word = node.surface
         hinsi = node.feature.split(",")[0]
         if hinsi == "名詞":
-            print(word + ":" + hinsi)
+            words.append(word)
         node = node.next
 
-text = "私はラーメンが好きです"
-split_text_noun(text)
+    return words
+
+text = "令和元年硬貨と天皇陛下即位の記念硬貨の打ち初め式が、大阪市北区の造幣局で行われました"
+split_text = split_text_noun(text)
+print(split_text)
