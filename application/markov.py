5cef95e39377389212d2fa69354d3b4a19122d4d
kab0718 <kab3nan9@gmail.com>
2019-12-09 16:27:14 +0900

    精度がばがば文章生成

--- application/markov.py
+++ application/markov.py
@@ -1,5 +1,6 @@
 import application.morpheme_common
 import pprint
+import random
 
 def file_open(file_name):
     test_data = open(file_name, encoding="utf_8_sig")
@@ -33,9 +34,30 @@ def generate_block(corps):
             blocks.append(block)
     return blocks
 
+def generate_text(blocks):
+    top_blocks = []
+    now_blocks = []
+
+    for block in blocks:
+        if(block[0] == '*'):
+            top_blocks.append(block)
+    sentences_top = top_blocks[random.randint(0, len(top_blocks) - 1)]
+    sentence = ''.join(sentences_top)
+    print(sentence)
+
+    while sentence[-1] != '*':
+        block_last = sentence[-1]
+        for block in blocks:
+            if(block[0] == block_last):
+                now_blocks.append(block)
+        add_block = now_blocks[random.randint(0, len(now_blocks) - 1)]
+        sentence = sentence + ''.join(add_block[1:])
+        print(sentence)
+
+
 if __name__ == '__main__':
     contents = file_open('serif/kitazawa_serif.txt')    #contentsはテキストファイルの一行が一要素となったリスト
     sentences = marking_text(contents)    #ひとまず文の先頭と最後に目印となる*を付与。余裕あれば一文じゃなくて。で区切る
     corps = analysis(sentences)    #corpsは文章を形態素解析したもの
     blocks = generate_block(corps)    #blocksは品詞ごとに分解したものを三単語ごとのブロックに分けてあるリスト
-    pprint.pprint(blocks)
+    generate_text(blocks)
