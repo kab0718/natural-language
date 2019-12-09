1c2ae7c1430661996d274e75b4df2aa307fece22
kab0718 <kab3nan9@gmail.com>
2019-12-09 14:48:49 +0900

    マルコフ連鎖下準備(マーキング)

--- application/markov.py
+++ application/markov.py
@@ -8,6 +8,13 @@ def file_open(file_name):
 
     return contents
 
+def marking_text(contents):
+    sentences = []
+    for content in contents:
+        sentence = '* ' + content + ' *'
+        sentences.append(sentence)
+    return sentences
+
 def analysis(contents):
     corps = []
     for content in contents:
@@ -26,6 +33,7 @@ def generate_block(corps):
 
 if __name__ == '__main__':
     contents = file_open('serif/kitazawa_data.txt')    #contentsはテキストファイルの一行が一要素となったリスト
-    corps = analysis(contents)    #corpsは文章を形態素解析したもの
+    sentences = marking_text(contents)    #ひとまず文の先頭と最後に目印となる*を付与。余裕あれば一文じゃなくて。で区切る
+    corps = analysis(sentences)    #corpsは文章を形態素解析したもの
     blocks = generate_block(corps)    #blocksは品詞ごとに分解したものを三単語ごとのブロックに分けてあるリスト
     print(blocks)
