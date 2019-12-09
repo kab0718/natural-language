e382395213a256f7a6bdec8e5a4bf02cb7f41a7f
kab0718 <kab3nan9@gmail.com>
2019-12-09 15:15:23 +0900

    マルコフ連鎖下準備(マーキング)修正

--- application/markov.py
+++ application/markov.py
@@ -1,4 +1,5 @@
 import application.morpheme_common
+import pprint
 
 def file_open(file_name):
     test_data = open(file_name, encoding="utf_8_sig")
@@ -11,14 +12,15 @@ def file_open(file_name):
 def marking_text(contents):
     sentences = []
     for content in contents:
-        sentence = '* ' + content + ' *'
+        sentence = '* ' + content + '*'    #開始と終了の目印
         sentences.append(sentence)
+
     return sentences
 
 def analysis(contents):
     corps = []
     for content in contents:
-        corps.append(application.morpheme_common.sharing_text_noun(content))
+        corps.append(application.morpheme_common.sharing_text_noun(content))    #形態素解析
 
     return corps
 
@@ -26,14 +28,14 @@ def generate_block(corps):
     blocks = []
     for corp in corps:
         list = corp.split()
-        for i in range(len(list)-3):
-            block = list[i:i+3]
+        for i in range(len(list)-2):
+            block = list[i:i+3]    #3単語ずつのブロック作成
             blocks.append(block)
     return blocks
 
 if __name__ == '__main__':
-    contents = file_open('serif/kitazawa_data.txt')    #contentsはテキストファイルの一行が一要素となったリスト
+    contents = file_open('serif/kitazawa_serif.txt')    #contentsはテキストファイルの一行が一要素となったリスト
     sentences = marking_text(contents)    #ひとまず文の先頭と最後に目印となる*を付与。余裕あれば一文じゃなくて。で区切る
     corps = analysis(sentences)    #corpsは文章を形態素解析したもの
     blocks = generate_block(corps)    #blocksは品詞ごとに分解したものを三単語ごとのブロックに分けてあるリスト
-    print(blocks)
+    pprint.pprint(blocks)
