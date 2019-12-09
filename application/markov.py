87ddfd32ef94255946d11f10cf38be7d62d5415c
kab0718 <kab3nan9@gmail.com>
2019-12-09 14:39:56 +0900

    3単語ごとのブロックに分割

--- application/markov.py
+++ application/markov.py
@@ -15,6 +15,17 @@ def analysis(contents):
 
     return corps
 
+def generate_block(corps):
+    blocks = []
+    for corp in corps:
+        list = corp.split()
+        for i in range(len(list)-3):
+            block = list[i:i+3]
+            blocks.append(block)
+    return blocks
+
 if __name__ == '__main__':
     contents = file_open('serif/kitazawa_data.txt')    #contentsはテキストファイルの一行が一要素となったリスト
     corps = analysis(contents)    #corpsは文章を形態素解析したもの
+    blocks = generate_block(corps)    #blocksは品詞ごとに分解したものを三単語ごとのブロックに分けてあるリスト
+    print(blocks)
